aaa5759e33bd37e2f97348bd9eadd935a35d17b5
R3fugee214 <huntersmith200@yahoo.com>
2014-03-14 08:15:08 -0700

    i added the x,y settings for thegame
    the game project is needing this aperently from your video

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -2,6 +2,6 @@
 // Create a game.PaddleEntity
  game.PaddleEntity=me.ObjectEntity.extend ({
     init: function(x,y, settings) {
-          this.parent(x,y, settings)
+          this.parent(x,y, settings);
     }
  });
